2677cc8fb67f78e042f86a3becb232061d679444
ToastedFlaco <aaron.arredondoar@outlook.com>
2021-04-19 19:28:36 -0500

    Added REST Controller for signup.

---
+++ src/main/java/com/github/toastedflaco/redditclon/controller/AuthController.java
@@ -0,0 +1,29 @@
+package com.github.toastedflaco.redditclon.controller;
+
+import com.github.toastedflaco.redditclon.dto.RegisterRequest;
+import com.github.toastedflaco.redditclon.service.AuthService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+@AllArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping(value = "/signup")
+    public ResponseEntity signup(@RequestBody RegisterRequest registerRequest) {
+        authService.signup(registerRequest);
+        return new ResponseEntity("Successfully registered your account",HttpStatus.OK);
+    }
+
+    @GetMapping("accountVerification/{token}")
+    public ResponseEntity<String> verifyAccount(@PathVariable String token) {
+        authService.verifyAccount(token);
+        return new ResponseEntity<>("Account Activated Successfully", HttpStatus.OK);
+    }
+
+}
