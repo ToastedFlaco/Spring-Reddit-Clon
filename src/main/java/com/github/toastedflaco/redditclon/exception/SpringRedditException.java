19194ba20700eb8dd12dcc13391e23962e2b75f5
ToastedFlaco <aaron.arredondoar@outlook.com>
2021-04-19 19:19:18 -0500

    Added exception handler for mail sending.

---
+++ src/main/java/com/github/toastedflaco/redditclon/exception/SpringRedditException.java
@@ -0,0 +1,7 @@
+package com.github.toastedflaco.redditclon.exception;
+
+public class SpringRedditException extends RuntimeException {
+    public SpringRedditException(String message) {
+        super(message);
+    }
+}
