fd39cf22b8d42ac297f34353b226d2b563cc10c4
ToastedFlaco <aaron.arredondoar@outlook.com>
2021-04-19 19:33:34 -0500

    Added Async annotation.

--- src/main/java/com/github/toastedflaco/redditclon/RedditclonApplication.java
+++ src/main/java/com/github/toastedflaco/redditclon/RedditclonApplication.java
@@ -2,8 +2,10 @@ package com.github.toastedflaco.redditclon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class RedditclonApplication {
 
     public static void main(String[] args) {
